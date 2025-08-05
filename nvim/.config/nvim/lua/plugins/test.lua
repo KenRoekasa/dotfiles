bec1d725b11d52832623abbab0b67623ccd3227e
roekasak <kenny.roekasa@hkbullion.com>
2025-08-05 09:18:04 +0000

    add ctest to nvim config

--- nvim/.config/nvim/lua/plugins/test.lua
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -7,12 +7,34 @@ return {
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "alfaix/neotest-gtest"
+      "alfaix/neotest-gtest",
+      "orjangj/neotest-ctest"
     },
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-gtest").setup({})
+          require("neotest-gtest").setup({}),
+          require("neotest-ctest").setup({
+            root = function(dir)
+              return require("neotest.lib").files.match_root_pattern(
+                "CMakePresets.json",
+                "compile_commands.json",
+                ".clangd",
+                ".clang-format",
+                ".clang-tidy",
+                "build",
+                "out",
+                ".git"
+              )(dir)
+            end,
+            is_test_file = function(file)
+              local is_cpp = file.ext == ".cpp" or file.ext == ".cc" or file.ext == ".cxx"
+              local has_test_in_name = string.find(file.stem, "Test") ~= nil
+              return is_cpp and has_test_in_name
+            end,
+            frameworks = { "gtest", "catch2", "doctest", "cpputest"},
+
+          })
         }
       })
     end
