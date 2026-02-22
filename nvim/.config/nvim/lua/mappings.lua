0c9f6acb3abf2ac1829cdb524cd3a861a6577fb8
Kenny Roekasa <kennykr1999@gmail.com>
2026-02-22 13:54:02 +0000

    add telescope keymaps and commands search hotkeys

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -6,6 +6,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
 
+-- Search
+map("n", "<leader>sk", "<cmd>Telescope keymaps<CR>", { desc = "Search keymaps" })
+map("n", "<leader>sa", "<cmd>Telescope commands<CR>", { desc = "Search commands" })
+
 -- CMake
 map("n", "<leader>cb", "<cmd>CMakeBuild<CR>", { desc = "Build" })
 map("n", "<leader>cc", "<cmd>CMakeGenerate<CR>", { desc = "CMake generate" })
